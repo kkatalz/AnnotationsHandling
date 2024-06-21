1a2a021305315f6d9bce97ef789de3619618555c
kkatalz <zlata.kiev2004@gmail.com>
2024-06-21 23:12:34 +0300

    Added two annotations

--- annotations/src/main/java/com/jintin/autofactory/AutoElement.java
+++
@@ -1,9 +0,0 @@
-package com.jintin.autofactory;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.SOURCE)
-@Target({ElementType.TYPE})
-public @interface AutoElement {
-    String value();
-}
